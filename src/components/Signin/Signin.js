59e609441e69bcbd2093e5d4b7c22dcac41b8d8d
Jochgem van Delft <jvandelft@pixsells.nl>
2018-04-11 13:08:25 +0800

    updated file

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -40,7 +40,7 @@ class Signin extends React.Component {
 				<main className="pa4 white-80">
 				  <div className="measure">
 				    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-				      <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+				      <legend className="f2 fw6 ph0 mh0">Sign In</legend>
 				      <div className="mt3">
 				        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
 				        <input 
